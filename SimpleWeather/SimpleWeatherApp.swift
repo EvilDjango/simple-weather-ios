4ba2ca02eb899c0222ecb678696a65928018fcf4
django <deerhunter0837@gmail.com>
2025-11-15 12:49:05 +0800

    Initial Commit

---
+++ SimpleWeather/SimpleWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWeatherApp.swift
+//  SimpleWeather
+//
+//  Created by Django on 2025/11/15.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
